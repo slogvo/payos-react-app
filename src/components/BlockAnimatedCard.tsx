5dc9f1ef5889e41769208fdb4ba81f16041403fe
slogvo <86196042+slogvo@users.noreply.github.com>
2025-01-03 17:55:44 +0700

    update nodelogo

--- src/components/BlockAnimatedCard.tsx
+++ src/components/BlockAnimatedCard.tsx
@@ -29,7 +29,7 @@ const Skeleton = () => {
     <div className="p-8 overflow-hidden h-full relative flex items-center justify-center">
       <div className="flex flex-row flex-shrink-0 justify-center items-center gap-2">
         <Container className="h-8 w-8 circle-1">
-          <GithubLogo className="h-4 w-4 " />
+          <NodeLogo className="h-4 w-4 " />
         </Container>
         <Container className="h-12 w-12 circle-2">
           <ReactLogo className="h-6 w-6 " />
@@ -182,19 +182,77 @@ const Container = ({
   );
 };
 
-export const GithubLogo = ({ className }: { className?: string }) => {
+export const NodeLogo = ({ className }: { className?: string }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="256"
+      height="292"
+      viewBox="0 0 256 292"
       className={className}
     >
+      <defs>
+        <linearGradient
+          id="logosNodejsIconAlt0"
+          x1="68.188%"
+          x2="27.823%"
+          y1="17.487%"
+          y2="89.755%"
+        >
+          <stop offset="0%" stopColor="#41873f" />
+          <stop offset="32.88%" stopColor="#418b3d" />
+          <stop offset="63.52%" stopColor="#419637" />
+          <stop offset="93.19%" stopColor="#3fa92d" />
+          <stop offset="100%" stopColor="#3fae2a" />
+        </linearGradient>
+        <linearGradient
+          id="logosNodejsIconAlt1"
+          x1="43.277%"
+          x2="159.245%"
+          y1="55.169%"
+          y2="-18.306%"
+        >
+          <stop offset="13.76%" stopColor="#41873f" />
+          <stop offset="40.32%" stopColor="#54a044" />
+          <stop offset="71.36%" stopColor="#66b848" />
+          <stop offset="90.81%" stopColor="#6cc04a" />
+        </linearGradient>
+        <linearGradient
+          id="logosNodejsIconAlt2"
+          x1="-4.389%"
+          x2="101.499%"
+          y1="49.997%"
+          y2="49.997%"
+        >
+          <stop offset="9.192%" stopColor="#6cc04a" />
+          <stop offset="28.64%" stopColor="#66b848" />
+          <stop offset="59.68%" stopColor="#54a044" />
+          <stop offset="86.24%" stopColor="#41873f" />
+        </linearGradient>
+        <path
+          id="logosNodejsIconAlt3"
+          d="M134.923 1.832c-4.344-2.443-9.502-2.443-13.846 0L6.787 67.801C2.443 70.244 0 74.859 0 79.745v132.208c0 4.887 2.715 9.502 6.787 11.945l114.29 65.968c4.344 2.444 9.502 2.444 13.846 0l114.29-65.968c4.344-2.443 6.787-7.058 6.787-11.945V79.745c0-4.886-2.715-9.501-6.787-11.944z"
+        />
+      </defs>
       <path
-        fill="currentColor"
-        fill-rule="evenodd"
-        d="M12 2C6.475 2 2 6.475 2 12a9.99 9.99 0 0 0 6.838 9.488c.5.087.687-.213.687-.476c0-.237-.013-1.024-.013-1.862c-2.512.463-3.162-.612-3.362-1.175c-.113-.288-.6-1.175-1.025-1.413c-.35-.187-.85-.65-.013-.662c.788-.013 1.35.725 1.538 1.025c.9 1.512 2.338 1.087 2.912.825c.088-.65.35-1.087.638-1.337c-2.225-.25-4.55-1.113-4.55-4.938c0-1.088.387-1.987 1.025-2.687c-.1-.25-.45-1.275.1-2.65c0 0 .837-.263 2.75 1.024a9.3 9.3 0 0 1 2.5-.337c.85 0 1.7.112 2.5.337c1.912-1.3 2.75-1.024 2.75-1.024c.55 1.375.2 2.4.1 2.65c.637.7 1.025 1.587 1.025 2.687c0 3.838-2.337 4.688-4.562 4.938c.362.312.675.912.675 1.85c0 1.337-.013 2.412-.013 2.75c0 .262.188.574.688.474A10.02 10.02 0 0 0 22 12c0-5.525-4.475-10-10-10"
+        fill="url(#logosNodejsIconAlt0)"
+        d="M134.923 1.832c-4.344-2.443-9.502-2.443-13.846 0L6.787 67.801C2.443 70.244 0 74.859 0 79.745v132.208c0 4.887 2.715 9.502 6.787 11.945l114.29 65.968c4.344 2.444 9.502 2.444 13.846 0l114.29-65.968c4.344-2.443 6.787-7.058 6.787-11.945V79.745c0-4.886-2.715-9.501-6.787-11.944z"
+      />
+      <mask id="logosNodejsIconAlt4" fill="#fff">
+        <use href="#logosNodejsIconAlt3" />
+      </mask>
+      <path
+        fill="url(#logosNodejsIconAlt1)"
+        d="M249.485 67.8L134.65 1.833c-1.086-.542-2.443-1.085-3.529-1.357L2.443 220.912c1.086 1.357 2.444 2.443 3.8 3.258l114.834 65.968c3.258 1.9 7.059 2.443 10.588 1.357L252.47 70.515c-.815-1.086-1.9-1.9-2.986-2.714"
+        mask="url(#logosNodejsIconAlt4)"
+      />
+      <mask id="logosNodejsIconAlt5" fill="#fff">
+        <use href="#logosNodejsIconAlt3" />
+      </mask>
+      <path
+        fill="url(#logosNodejsIconAlt2)"
+        d="M249.756 223.898c3.258-1.9 5.701-5.158 6.787-8.687L130.579.204c-3.258-.543-6.787-.272-9.773 1.628L6.786 67.53l122.979 224.238c1.628-.272 3.529-.815 5.158-1.63z"
+        mask="url(#logosNodejsIconAlt5)"
       />
     </svg>
   );
