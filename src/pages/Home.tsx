a001e4e1d2537a0b451e810390338e6aec73cef1
slogvo <86196042+slogvo@users.noreply.github.com>
2025-01-04 11:12:16 +0700

    update style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -140,12 +140,14 @@ const Home = () => {
         <BackgroundBeamsWithCollision>
           <div className="flex flex-col sm:flex-row justify-center items-center">
             <h3 className="text-2xl max-w-md relative z-20 md:text-3xl lg:text-5xl font-bold text-center text-white font-sans tracking-tight">
-              Hi, I'm Long
+              <span className="drop-shadow-[0px_0px_40px_rgba(0,213,255,1)]">
+                Hi, I'm Long
+              </span>
               <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
                 <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-sky-500 via-blue-400 to-sky-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
                   <span className="">A Web Developer</span>
                 </div>
-                <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-sky-500 via-blue-400 to-sky-500   py-4">
+                <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-sky-500 via-blue-400 to-sky-500 py-4">
                   <span className="">A Web Developer</span>
                 </div>
               </div>
